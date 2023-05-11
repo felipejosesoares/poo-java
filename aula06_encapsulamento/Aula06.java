fb0a746881ce174991b1b5f8a7e05133c93c0416
FeliipeJ <felipejose.s1994@gmail.com>
2023-05-11 00:39:54 -0300

    seguindo

---
+++ aula06_encapsulamento/Aula06.java
@@ -0,0 +1,12 @@
+package aula06_encapsulamento;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
